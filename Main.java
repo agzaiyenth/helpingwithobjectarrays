e2d84c450fc9f8bf607331854415730b9fab5e41
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-02-13 22:35:50 +0530

    new edit
    final completed

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 import java.util.*;
 public class Main {
+
+    private static Ticket [] tickets=new Ticket[52];
+    private static int ticketCount=0;
+
+    private static int[][] seats = new int[4][];
+
+
+
+
+
+
     public int user_menu(){
+        System.out.println("\n\n");
         System.out.println("*".repeat(50));
         System.out.println("*"+" ".repeat(18)+"MENU OPTIONS"+" ".repeat(18)+"*");
         System.out.println("*".repeat(50));
@@ -13,22 +25,34 @@ public class Main {
         System.out.println(" ".repeat(5)+"0) Quit");
         System.out.println("*".repeat(50));
         System.out.print("Please select an option: ");
-        int response=new Scanner(System.in).nextInt();
+        int response= new Scanner(System.in).nextInt();
+        System.out.println("\n\n");
         return response;
     }
 
-    public void cancel_seat(int seats[][]){
+    public void cancel_seat(int[][] seats){
         System.out.print("Please Enter the row letter: ");
         String row_letter=new Scanner(System.in).next().toLowerCase();
         System.out.print("Enter the Seat number: ");
         int seat_number=new Scanner(System.in).nextInt();
-        System.out.println( row_letter+" "+seat_number);
         switch (row_letter){
             case "a":
                 if(seat_number>0 && seat_number<15){
                     if(seats[0][seat_number-1]==1){
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully Cancelled.");
                         seats[0][seat_number-1]=0;
+                        for (int i = 0; i < ticketCount; i++) {
+                            Ticket ticket = tickets[i];
+                            if (Objects.equals(ticket.getRow(), "A") && ticket.getSeat() == seat_number) {
+                                for (int j = i; j < ticketCount - 1; j++) {
+                                    tickets[j] = tickets[j + 1];
+                                }
+                                tickets[ticketCount - 1] = null;
+                                ticketCount--;
+                                System.out.println("Ticket canceled successfully!");
+
+                            }
+                        }
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is available already.");
 
@@ -42,6 +66,18 @@ public class Main {
                     if(seats[1][seat_number-1]==1){
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully Cancelled.");
                         seats[1][seat_number-1]=0;
+                        for (int i = 0; i < ticketCount; i++) {
+                            Ticket ticket = tickets[i];
+                            if (Objects.equals(ticket.getRow(), "B") && ticket.getSeat() == seat_number) {
+                                for (int j = i; j < ticketCount - 1; j++) {
+                                    tickets[j] = tickets[j + 1];
+                                }
+                                tickets[ticketCount - 1] = null;
+                                ticketCount--;
+                                System.out.println("Ticket canceled successfully!");
+
+                            }
+                        }
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is available already.");
 
@@ -55,6 +91,18 @@ public class Main {
                     if(seats[2][seat_number-1]==1){
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully Cancelled.");
                         seats[2][seat_number-1]=0;
+                        for (int i = 0; i < ticketCount; i++) {
+                            Ticket ticket = tickets[i];
+                            if (Objects.equals(ticket.getRow(), "C") && ticket.getSeat() == seat_number) {
+                                for (int j = i; j < ticketCount - 1; j++) {
+                                    tickets[j] = tickets[j + 1];
+                                }
+                                tickets[ticketCount - 1] = null;
+                                ticketCount--;
+                                System.out.println("Ticket canceled successfully!");
+
+                            }
+                        }
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is available already.");
 
@@ -68,6 +116,18 @@ public class Main {
                     if(seats[3][seat_number-1]==1){
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully Cancelled.");
                         seats[3][seat_number-1]=0;
+                        for (int i = 0; i < ticketCount; i++) {
+                            Ticket ticket = tickets[i];
+                            if (Objects.equals(ticket.getRow(), "D") && ticket.getSeat() == seat_number) {
+                                for (int j = i; j < ticketCount - 1; j++) {
+                                    tickets[j] = tickets[j + 1];
+                                }
+                                tickets[ticketCount - 1] = null;
+                                ticketCount--;
+                                System.out.println("Ticket canceled successfully!");
+
+                            }
+                        }
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is available already.");
 
@@ -82,18 +142,42 @@ public class Main {
 
 
     }
-    public void buy_seat(int seats[][]){
+
+    public double price_check(int seat_number){
+        if(seat_number>0&&seat_number<6){
+            return 200;
+        } else if (seat_number>5&&seat_number<10) {
+            return 150;
+        }else{
+            return 180;
+        }
+    }
+    public void buy_seat(int[][] seats){
+        System.out.print("Please Enter your name: ");
+        String name=new Scanner(System.in).next();
+        System.out.print("Please Enter your surname: ");
+        String surname=new Scanner(System.in).next();
+        System.out.print("Please Enter your email: ");
+        String email=new Scanner(System.in).next();
         System.out.print("Please Enter the row letter: ");
         String row_letter=new Scanner(System.in).next().toLowerCase();
         System.out.print("Enter the Seat number: ");
         int seat_number=new Scanner(System.in).nextInt();
-        System.out.println( row_letter+" "+seat_number);
+
+        double price=price_check(seat_number);
         switch (row_letter){
             case "a":
                 if(seat_number>0 && seat_number<15){
                     if(seats[0][seat_number-1]==0){
-                        System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully purchased");
                         seats[0][seat_number-1]=1;
+                        Person person = new Person(name, surname, email);
+                        Ticket ticket = new Ticket("A", seat_number-1, price, person);
+                        if (ticketCount < 52) {
+                            tickets[ticketCount] = ticket;
+                            ticketCount++;
+                            ticket.save();
+                            System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" Ticket bought successfully!");
+                        }
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is not available.");
 
@@ -105,8 +189,16 @@ public class Main {
             case "b":
                 if(seat_number>0 && seat_number<13){
                     if(seats[1][seat_number-1]==0){
-                        System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully purchased");
                         seats[1][seat_number-1]=1;
+                        Person person = new Person(name, surname, email);
+                        Ticket ticket = new Ticket("B", seat_number-1, price, person);
+                        if (ticketCount < 52) {
+                            tickets[ticketCount] = ticket;
+                            ticketCount++;
+                            ticket.save();
+                            System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" Ticket bought successfully!");
+                        }
+
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is not available.");
 
@@ -118,8 +210,15 @@ public class Main {
             case "c":
                 if(seat_number>0 && seat_number<13){
                     if(seats[2][seat_number-1]==0){
-                        System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully purchased");
                         seats[2][seat_number-1]=1;
+                        Person person = new Person(name, surname, email);
+                        Ticket ticket = new Ticket("C", seat_number-1, price, person);
+                        if (ticketCount < 52) {
+                            tickets[ticketCount] = ticket;
+                            ticketCount++;
+                            ticket.save();
+                            System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" Ticket bought successfully!");
+                        }
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is not available.");
 
@@ -131,8 +230,15 @@ public class Main {
             case "d":
                 if(seat_number>0 && seat_number<15){
                     if(seats[3][seat_number-1]==0){
-                        System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" has succesfully purchased");
                         seats[3][seat_number-1]=1;
+                        Person person = new Person(name, surname, email);
+                        Ticket ticket = new Ticket("D", seat_number-1, price, person);
+                        if (ticketCount < 52) {
+                            tickets[ticketCount] = ticket;
+                            ticketCount++;
+                            ticket.save();
+                            System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" Ticket bought successfully!");
+                        }
                     }else{
                         System.out.println("Seat "+row_letter.toUpperCase()+" "+seat_number+" is not available.");
 
@@ -147,12 +253,12 @@ public class Main {
 
     }
 
-    public void find_first_available(int seats[][]){
+    public void find_first_available(int[][] seats){
         int x=0;
         int n;
         boolean state=false;
         String row_letter=" ";
-        for(int row[]:seats){
+        for(int[] row :seats){
             switch(x){
                 case 0:
                     row_letter="A";
@@ -187,9 +293,9 @@ public class Main {
         }
     }
 
-    public void show_seating_plan(int seats[][]){
+    public static void show_seating_plan(int[][] seats){
         int x=0;
-        for (int row[]:seats){
+        for (int[] row :seats){
             if(x==2){
                 System.out.println();
             }
@@ -205,14 +311,92 @@ public class Main {
         }
     }
 
+    public static void print_tickets_info() {
+        double totalAmount = 0;
+        System.out.println(ticketCount);
+
+        System.out.print("\nTickets Sold During Session: \n");
+        for (int i = 0; i < ticketCount; i++) {
+            Ticket ticket = tickets[i];
+            System.out.println("\nTicket " + (i + 1) + ":");
+            ticket.printInfo();
+            totalAmount += ticket.getPrice();
+        }
+        System.out.println("\nTotal Sales: £"+(int) totalAmount);
+    }
+
+    public static void search_ticket() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the row letter: ");
+        String rowLetter = scanner.next().toUpperCase();
+        System.out.print("Enter the seat number: ");
+        int seatNumber = scanner.nextInt();
+
+        boolean found = false;
+
+        // Check if the seat is available in the seats array
+        switch (rowLetter) {
+            case "A":
+                if (seatNumber > 0 && seatNumber < 15 && seats[0][seatNumber - 1] == 0) {
+                    System.out.println("This seat is available.");
+                    found = true;
+                }
+                break;
+            case "B":
+                if (seatNumber > 0 && seatNumber < 13 && seats[1][seatNumber - 1] == 0) {
+                    System.out.println("This seat is available.");
+                    found = true;
+                }
+                break;
+            case "C":
+                if (seatNumber > 0 && seatNumber < 13 && seats[2][seatNumber - 1] == 0) {
+                    System.out.println("This seat is available.");
+                    found = true;
+                }
+                break;
+            case "D":
+                if (seatNumber > 0 && seatNumber < 15 && seats[3][seatNumber - 1] == 0) {
+                    System.out.println("This seat is available.");
+                    found = true;
+                }
+                break;
+            default:
+                System.out.println("Invalid row letter.");
+        }
+
+        // If the seat is not found in the seats array, check the tickets array
+        if (!found) {
+            System.out.println("This seat is unavailable.");
+            String row;
+            int seatno;
+            for (int i = 0; i < ticketCount; i++) {
+                Ticket ticket = tickets[i];
+                row= ticket.getRow();
+                seatno=ticket.getSeat();
+
+                if(Objects.equals(row, rowLetter) && seatno==seatNumber-1){
+                    ticket.printInfo();
+                }
+
+            }
+
+        }
+
+
+    }
+
+
     public static void main(String[] args) {
-        Person obj=new Person();
+
         System.out.println("Welcome to the Plane Management System");
-        int seats[][] = new int[4][];
         seats[0] = new int[14];
         seats[1] = new int[12];
         seats[2] = new int[12];
         seats[3] = new int[14];
+
+
+
         int response;
 
         do{
@@ -222,7 +406,9 @@ public class Main {
                 case 1: new Main().buy_seat(seats); break;
                 case 2: new Main().cancel_seat(seats); break;
                 case 3: new Main().find_first_available(seats); break;
-                case 4: new Main().show_seating_plan(seats); break;
+                case 4: show_seating_plan(seats); break;
+                case 5: print_tickets_info(); break;
+                case 6: search_ticket(); break;
 
             }
         }while (response!=0);
